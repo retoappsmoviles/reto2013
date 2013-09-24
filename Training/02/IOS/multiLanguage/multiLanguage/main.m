37e5b01aba2ff39f96cc210d64f389af18bfbcd0
carlos <carcoco@gmail.com>
2013-09-24 19:34:52 +0200

    Initial Commit

---
+++ Training/02/IOS/multiLanguage/multiLanguage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  multiLanguage
+//
+//  Created by carlos on 24/09/13.
+//  Copyright (c) 2013 carlos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
