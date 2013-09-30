99506770a90d3f139dce3f539a1cc9c49db77142
Edgar Miro <edgarmiro@gmail.com>
2013-09-30 16:15:34 +0200

    Tabs

---
+++ Training/03/Android/src/es/com/blogspot/retoappsmoviles/training03/Fragment1.java
@@ -0,0 +1,26 @@
+package es.com.blogspot.retoappsmoviles.training03;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment1 extends Fragment{	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment1, null);
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+}
